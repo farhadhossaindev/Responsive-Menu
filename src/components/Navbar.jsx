6874e41a5d793a6f2190e3c66a40fd48520ed2f0
Farhad Hossain <dev.farhadhossain@gmail.com>
2024-01-11 16:14:19 +0600

    02

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import logo from "../assets/logo.png"
+import logo from "../assets/Logo.png"
 import { Link, } from 'react-router-dom'
 import NavLinks from './NavLinks'
 import Button from './button'
